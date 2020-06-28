31e5f7b18cd6402b8bd5d5d828bf203ee1d9ef14
Santiago Chopitea <elchopi26@gmail.com>
2020-06-27 23:48:43 -0300

    Se agrega router y usuarioSchema

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const conectarDB = require('./config/db');
+
 const app = express();
+conectarDB();
+
+//Habilitar express.json
+app.use(express.json({ extended: true }));
+
 //Puerto de la app
 const PORT = process.env.PORT || 4000;
+
+//Importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
 
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en el puerto ${PORT}`);
