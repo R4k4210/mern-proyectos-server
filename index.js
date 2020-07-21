732cd6399811dfd4d5fd0a2fcd02c32d7b2f0520
Santiago Chopitea <elchopi26@gmail.com>
2020-07-21 00:39:45 -0300

    Se cambia la variable port

--- index.js
+++ index.js
@@ -11,15 +11,12 @@ app.use(cors());
 //Habilitar express.json
 app.use(express.json({ extended: true }));
 
-//Puerto de la app
-const port = process.env.port || 4000;
-
 //Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(process.env.PORT || 4000, "0.0.0.0", () => {
     console.log(`Servidor funcionando en el puerto ${port}`);
 })
